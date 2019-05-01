2f06d28a56be51ce7724faba686b9f20afe6b3f3
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-01 16:15:24 -0500

    typo in data filename

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -10,6 +10,7 @@ fun main() {
 fun getXML(filename: String): Document {
     val builder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder()
     val file = File(filename)
+    //TODO: handle file not found
     val document = builder.parse(file)
     document.documentElement.normalize()
     return document
