deac442ae37cd10a06fcaaf50a0d58af688b195a
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-01 16:08:25 -0500

    Added relic data

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,11 @@
-fun main(args: Array<String>) {
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+
+fun main() {
     println("Hello World!")
+}
+
+fun getXML(filename: String) {
+    var builder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder()
+    var file = File("relic-data.xml")
 }
