64f9bbd193678e4935ed9d6ae57ace432d58ad7d
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-01 16:40:43 -0500

    exploratory tag logging

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,10 +1,19 @@
 import org.w3c.dom.Document
+import org.w3c.dom.Element
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
 fun main() {
     val relicDoc = getXML("relic-data.xml")
     println("Root element: ${relicDoc.documentElement.nodeName}")
+
+    val relicList = relicDoc.getElementsByTagName("tr")
+    println("Elements in the list: ${relicList.length}")
+    println("Example of relic data: ${relicList.item(0)}")
+    for (i in 0 until relicList.length) {
+        val node = relicList.item(i) as Element
+        println("Element name at ${i+1}: ${node.tagName}")
+    }
 }
 
 fun getXML(filename: String): Document {
