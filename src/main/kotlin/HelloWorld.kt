c3116d32c5921e009ebd7707a07245975e953bc2
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-02 14:12:07 -0500

    Finished the abomination that loads the relic information into a list for further processing

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -7,13 +7,29 @@ fun main() {
     val relicDoc = getXML("relic-data.xml")
     println("Root element: ${relicDoc.documentElement.nodeName}")
 
+    val relics:MutableList<Relic> = mutableListOf()
+
     val relicList = relicDoc.getElementsByTagName("tr")
-    println("Elements in the list: ${relicList.length}")
-    println("Example of relic data: ${relicList.item(0)}")
     for (i in 0 until relicList.length) {
         val node = relicList.item(i) as Element
         println("Element name at ${i+1}: ${node.tagName}")
+        println("first child: ${node.firstChild.nodeName}")
+        relics.add(Relic(
+            era = node.firstChild.firstChild.textContent.trim(),
+            name = node.firstChild.nextSibling.firstChild.firstChild.textContent,
+            commonRewards = mutableListOf(
+                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim(),
+                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.textContent.trim()
+            ),
+            uncommonRewards = mutableListOf(
+                node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+                node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim()
+            ),
+            rareReward = node.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim()
+        ))
     }
+    println("resulting list size: ${relics.size}")
 }
 
 fun getXML(filename: String): Document {
---
+++ src/main/kotlin/Relic.kt
@@ -0,0 +1,7 @@
+data class Relic(
+    val era: String,
+    val name: String,
+    val commonRewards: MutableList<String>,
+    val uncommonRewards: MutableList<String>,
+    val rareReward: String
+)
