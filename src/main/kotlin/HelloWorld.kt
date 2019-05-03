68e9f4bfc564e5ee0c3785c635940dde22348b16
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-03 17:30:54 -0500

    Pulled HTML parsing into its own function

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,5 +1,6 @@
 import org.w3c.dom.Document
 import org.w3c.dom.Element
+import org.w3c.dom.NodeList
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -7,29 +8,10 @@ fun main() {
     val relicDoc = getXML("relic-data.xml")
     println("Root element: ${relicDoc.documentElement.nodeName}")
 
-    val relics:MutableList<Relic> = mutableListOf()
+    val relics: MutableList<Relic> = mutableListOf()
 
     val relicList = relicDoc.getElementsByTagName("tr")
-    for (i in 0 until relicList.length) {
-        val node = relicList.item(i) as Element
-        println("Element name at ${i+1}: ${node.tagName}")
-        println("first child: ${node.firstChild.nodeName}")
-        relics.add(Relic(
-            era = node.firstChild.firstChild.textContent.trim(),
-            name = node.firstChild.nextSibling.firstChild.firstChild.textContent,
-            commonRewards = mutableListOf(
-                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
-                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim(),
-                node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.textContent.trim()
-            ),
-            uncommonRewards = mutableListOf(
-                node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
-                node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim()
-            ),
-            rareReward = node.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim()
-        ))
-    }
-    println("resulting list size: ${relics.size}")
+
 }
 
 fun getXML(filename: String): Document {
@@ -39,4 +21,30 @@ fun getXML(filename: String): Document {
     val document = builder.parse(file)
     document.documentElement.normalize()
     return document
+}
+
+fun parseRelicTable(relicList: NodeList): MutableList<Relic> {
+    val relics = mutableListOf<Relic>()
+    for (i in 0 until relicList.length) {
+        val node = relicList.item(i) as Element
+        println("Element name at ${i + 1}: ${node.tagName}")
+        println("first child: ${node.firstChild.nodeName}")
+        relics.add(
+            Relic(
+                era = node.firstChild.firstChild.textContent.trim(),
+                name = node.firstChild.nextSibling.firstChild.firstChild.textContent,
+                commonRewards = mutableListOf(
+                    node.firstChild.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+                    node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim(),
+                    node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.textContent.trim()
+                ),
+                uncommonRewards = mutableListOf(
+                    node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+                    node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim()
+                ),
+                rareReward = node.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim()
+            )
+        )
+    }
+    println("resulting list size: ${relics.size}")
 }
