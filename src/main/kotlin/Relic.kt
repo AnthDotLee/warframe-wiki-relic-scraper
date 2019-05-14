c2a213ff5e7622454ce737bb0742bfbfac57a311
Anthony Lee <anthony.mi.lee@outlook.com>
2019-05-14 11:09:21 -0500

    created XML for vaulted relic data

--- src/main/kotlin/Relic.kt
+++ src/main/kotlin/Relic.kt
@@ -1,8 +1,8 @@
 data class Relic(
-    val era: String,
-    val name: String,
-    val commonRewards: MutableList<String>,
-    val uncommonRewards: MutableList<String>,
-    val rareReward: String,
-    val isVaulted: Boolean = false
+val era: String,
+val name: String,
+val commonRewards: MutableList<String>,
+val uncommonRewards: MutableList<String>,
+val rareReward: String,
+val isVaulted: Boolean = false
 )
--- src/main/kotlin/RelicDatabaseLoader.kt
+++ src/main/kotlin/RelicDatabaseLoader.kt
@@ -9,84 +9,86 @@ import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
 fun main() {
-    val relicDoc = getXML("relic-data.xml")
-    println("Root element: ${relicDoc.documentElement.nodeName}")
+val relicDoc = getXML("relic-data.xml")
+println("Root element: ${relicDoc.documentElement.nodeName}")
 
-    val relics: MutableList<Relic> = parseRelicTable(relicDoc.getElementsByTagName("tr"))
-    try {
-        saveRelicsToDB(relics)
-    } catch (ex: Exception) {
-        println(ex)
-    }
+val relics: MutableList<Relic> = parseRelicTable(relicDoc.getElementsByTagName("tr"))
+try {
+saveRelicsToDB(relics)
+} catch (ex: Exception) {
+println(ex)
+}
 }
 
 fun getXML(filename: String): Document {
-    val builder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder()
-    val file = File(filename)
-    //TODO: handle file not found
-    val document = builder.parse(file)
-    document.documentElement.normalize()
-    return document
+val builder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder()
+val file = File(filename)
+//TODO: handle file not found
+val document = builder.parse(file)
+document.documentElement.normalize()
+return document
 }
 
 fun saveRelicsToDB(relics: Iterable<Relic>) {
-    // Drops the whole collection and re-inserts the data
+// Drops the whole collection and re-inserts the data
 
-    var mongoClient: MongoClient? = null
-    try {
-        mongoClient = MongoClients.create("mongodb://127.0.0.1:27017")
-        println("Connected to MongoDB")
-    } catch(ex: MongoException) {
-        ex.printStackTrace()
-    }
+var mongoClient: MongoClient? = null
+try {
+mongoClient = MongoClients.create("mongodb://127.0.0.1:27017")
+println("Connected to MongoDB")
+} catch(ex: MongoException) {
+ex.printStackTrace()
+}
 
-    val mongoDB = mongoClient!!.getDatabase("warframe-relic-db")
+val mongoDB = mongoClient!!.getDatabase("warframe-relic-db")
 
-    // Drop the existing collection
-    val collection = mongoDB.getCollection("relics")
-    collection.drop()
+// Drop the existing collection
+val collection = mongoDB.getCollection("relics")
+collection.drop()
 
-    // Insert relic using a default vaulted status
-    for(relic in relics) {
-        val relicDoc = org.bson.Document("name", relic.name)
-            .append("era", relic.era)
-            .append("rewards", org.bson.Document("common", relic.commonRewards)
-                .append("uncommon", relic.uncommonRewards)
-                .append("rare", relic.rareReward))
-            .append("vaulted", relic.isVaulted)
-        collection.insertOne(relicDoc)
-    }
+// Insert relic using a default vaulted status
+for(relic in relics) {
+val relicDoc = org.bson.Document("name", relic.name)
+.append("era", relic.era)
+.append("rewards", org.bson.Document("common", relic.commonRewards)
+.append("uncommon", relic.uncommonRewards)
+.append("rare", relic.rareReward))
+.append("vaulted", relic.isVaulted)
+collection.insertOne(relicDoc)
+}
+
+// Update the Vaulted status
 }
 
 fun parseRelicTable(relicList: NodeList): MutableList<Relic> {
-    val relics = mutableListOf<Relic>()
-    try {
-        for (i in 0 until relicList.length) {
-            val node = relicList.item(i) as Element
-            relics.add(
-                Relic(
-                    era = node.firstChild.firstChild.textContent.trim(),
-                    name = node.firstChild.nextSibling.firstChild.firstChild.textContent,
-                    commonRewards = mutableListOf(
-                        node.firstChild.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
-                        node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim(),
-                        node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.textContent.trim()
-                    ),
-                    uncommonRewards = mutableListOf(
-                        node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
-                        node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim()
-                    ),
-                    rareReward = node.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim()
-                )
-            )
-        }
-    } catch (exception: Exception) {
-        println("Unable to parse table into list. Ex: $exception")
-        return mutableListOf()
-    }
-    return relics
+val relics = mutableListOf<Relic>()
+try {
+for (i in 0 until relicList.length) {
+val node = relicList.item(i) as Element
+relics.add(
+Relic(
+era = node.firstChild.firstChild.textContent.trim(),
+name = node.firstChild.nextSibling.firstChild.firstChild.textContent,
+commonRewards = mutableListOf(
+node.firstChild.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim(),
+node.firstChild.nextSibling.nextSibling.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.textContent.trim()
+),
+uncommonRewards = mutableListOf(
+node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim(),
+node.firstChild.nextSibling.nextSibling.nextSibling.firstChild.firstChild.nextSibling.firstChild.firstChild.textContent.trim()
+),
+rareReward = node.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.firstChild.firstChild.firstChild.textContent.trim()
+)
+)
+}
+} catch (exception: Exception) {
+println("Unable to parse table into list. Ex: $exception")
+return mutableListOf()
+}
+return relics
 }
 
 fun updateVaultedStatus() {
-    throw NotImplementedError("Not done yet")
+throw NotImplementedError("Not done yet")
 }
